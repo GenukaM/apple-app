8fc6b8a2856ffb09d4eaee4c4f772edc8aea316c
genuka manthila <genukamanthila21@gmail.com>
2025-11-26 23:43:38 +0530

    feat : new 3d models files

---
+++ src/components/StudioLights.tsx
@@ -0,0 +1,6 @@
+
+export const StudioLights = () => {
+  return (
+    <div>StudioLights</div>
+  )
+}
---
+++ src/components/models/index.js
@@ -0,0 +1,9 @@
+import MacbookModel14 from '../models/Macbook-14';
+import MacbookModel16 from '../models/Macbook-16';
+import MacbookModel from '../models/Macbook';
+ 
+export  {
+    MacbookModel,
+    MacbookModel14,
+    MacbookModel16
+}
